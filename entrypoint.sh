278c8cce65dd0cb6b8150ed73f60a202a2f6aa3c
D. Sebastien Taggart <taggart_daniel@bah.com>
2018-07-26 10:11:31 -0400

    Add check to see if ACCOUNT (account name) already exists in the config.json, and if so, do not add it again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,20 +20,25 @@ source .env
 #  ELSE demo mode all the way :)
 
 # Configure the AWS CLI inside the container
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region $AWS_DEFAULT_REGION
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set region ${AWS_DEFAULT_REGION}
 aws configure set output json
 
-# Generate config.json with our account settings
-pipenv run python cloudmapper.py configure add-account --config-file config.json --name $ACCOUNT --name $ACCOUNT --id $ACCOUNT --default true
+if grep -q "${ACCOUNT}" config.json
+then
+    echo "${ACCOUNT} already exists in config.json, skipping 'configure add-account'"
+else
+    # Generate config.json with our account settings
+    pipenv run python cloudmapper.py configure add-account --config-file config.json --name ${ACCOUNT} --name ${ACCOUNT} --id ${ACCOUNT} --default true
+fi
 
 # Collect info about our AWS infrastructure, store this in /account-data, which
 # is volumed to the host.
-pipenv run python cloudmapper.py collect --account $ACCOUNT
+pipenv run python cloudmapper.py collect --account ${ACCOUNT}
 
 # Prepare the collected data for serving
-pipenv run python cloudmapper.py prepare --account $ACCOUNT
+pipenv run python cloudmapper.py prepare --account ${ACCOUNT}
 
 # Start serving on :8000 (by default), --public means bind to 0.0.0.0
 pipenv run python cloudmapper.py webserver --public
